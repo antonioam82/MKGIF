d6c65bc007f55d9cc2174adaa6d78365342429b7
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-04-15 15:32:07 +0200

    Update mkgif3.py

--- mkgif3.py
+++ mkgif3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from moviepy.editor import VideoFileClip
-#import sys
 from PIL import Image
 import pyfiglet
 import ffmpeg
@@ -25,20 +24,27 @@ c_index = color[random.randint(0,6)]
 b_index = bright[random.randint(0,2)]
 
 def main():
-
-    parser = argparse.ArgumentParser(prog="MKGIF 2.0",conflict_handler='resolve',description="Create gifs from videos in command line or convert '.webp' files into '.gif'.",
+    # INTRODUCCIÓN DE ARGUMENTOS
+    parser = argparse.ArgumentParser(prog="MKGIF 2.0",conflict_handler='resolve',
+                                     description="Create gifs from videos in command line or convert '.webp' files into '.gif'.",
                                      epilog = "REPO: https://github.com/antonioam82/MKGIF")
-    parser.add_argument('-src','--source',required=True,type=str,help='Ruta archivo original')
-    parser.add_argument('-dest','--destination',default='my_gif.gif',type=str,help='Ruta archivo destino')
+    parser.add_argument('-src','--source',required=True,type=str,help='Nombre archivo original')
+    parser.add_argument('-dest','--destination',default='my_gif.gif',type=str,help='Nombre archivo destino')
     parser.add_argument('-st','--start',default=0.0,type=float,help='Segundo inicial del gif')
     parser.add_argument('-e','--end',default=None,type=str,help='Segundo final del gif')
     parser.add_argument('-shw','--show',help='Mostrar resultado',action='store_true')
     parser.add_argument('-sz','--size',default=100,type=int,help='Tamaño relativo (100 por defecto)')
-    parser.add_argument('-spd','--speed',default=100,type=int,help='Velocidad relativa de animación (100 por defecto)')
+    parser.add_argument('-spd','--speed',default=100,type=check_positive,help='Velocidad relativa de animación (100 por defecto)')
     parser.add_argument('-fps','--fraps',default=None,type=int,help='Frames por segundo')
 
     args=parser.parse_args()
     gm(args)
+
+def check_positive(val):
+    ivalue = int(val)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("Speed value must be positive (%s is not valid)." % val)
+    return ivalue
 
 def show(f):
     print("GENERATING VIEW...")
@@ -87,7 +93,7 @@ def gm(args):
                         .subclip((0,args.start),
                              (0,duration))
                         .resize(args.size/100)
-                        .speedx(args.speed/100))
+                        .speedx(args.speed/100)) # TODO: Crear función para verificar que el valor de la velocidad es positivo
                         print('CREATING GIF...')
                         clip.write_gif(args.destination,fps=args.fraps)
                         clip.close()
@@ -104,7 +110,7 @@ def gm(args):
                         file.save(args.destination,'gif',save_all=True,background=0)
                         file.close()
                         size = get_size_format(os.stat(args.destination).st_size)
-                        print(f"Created '{args.destination}' with size {size} from '{args.source}'")
+                        print(f"Created '{args.destination}' with size {size} from '{args.source}'.")
                         if args.show:
                             show(args.destination)
                     else:
@@ -118,5 +124,3 @@ def gm(args):
 
 if __name__=='__main__':
     main()
-
-
