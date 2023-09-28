1a8d47cf3a865d42d205e59b3b671279d81dde1b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-09-28 12:13:42 +0200

    Update mkgif4.py

--- mkgif4.py
+++ mkgif4.py
@@ -11,63 +11,63 @@ import argparse
 import random
 from colorama import Fore, init, Style
 import os
-
+ 
 init()
 color = {0:Fore.RED,1:Fore.GREEN,2:Fore.YELLOW,
          3:Fore.BLUE,4:Fore.CYAN,5:Fore.MAGENTA,6:Fore.WHITE}
-
+ 
 bright = {0:Style.DIM,1:Style.NORMAL,2:Style.BRIGHT}
-
+ 
 c_index = color[random.randint(0,6)]
 b_index = bright[random.randint(0,2)]
-
+ 
 def main():
     global file_extension
-    parser = argparse.ArgumentParser(prog="MKGIF 2.1",conflict_handler='resolve',
+    parser = argparse.ArgumentParser(prog="MKGIF 2.2",conflict_handler='resolve',
                                      description="Create gifs from videos in command line or convert '.webp' files into '.gif'.",
                                      epilog = "REPO: https://github.com/antonioam82/MKGIF")
-    parser.add_argument('-src','--source',required=True,type=check_source_ext,help='Nombre archivo original')
-    parser.add_argument('-dest','--destination',default='my_gif.gif',type=check_result_ext,help='Nombre archivo destino')
-    parser.add_argument('-st','--start',default=0.0,type=check_time,help='Segundo inicial del gif')
-    parser.add_argument('-e','--end',default=None,type=check_time,help='Segundo final del gif')
-    parser.add_argument('-shw','--show',help='Mostrar resultado',action='store_true')
-    parser.add_argument('-sz','--size',default=100,type=check_positive,help='Tamaño relativo (100 por defecto)')
-    parser.add_argument('-spd','--speed',default=100,type=check_positive,help='Velocidad relativa de animación (100 por defecto)')
-    parser.add_argument('-fps','--fraps',default=None,type=int,help='Frames por segundo')
-
+    parser.add_argument('-src','--source',required=True,type=check_source_ext,help='Source file name')
+    parser.add_argument('-dest','--destination',default='my_gif.gif',type=check_result_ext,help='Destination file name')
+    parser.add_argument('-st','--start',default=0.0,type=check_time,help='Initial second of the gif')
+    parser.add_argument('-e','--end',default=None,type=check_time,help='End second of the gif')
+    parser.add_argument('-shw','--show',help='Generate gif and display the result',action='store_true')
+    parser.add_argument('-sz','--size',default=100,type=check_positive,help='Relative size of the gif (100 by default)')
+    parser.add_argument('-spd','--speed',default=100,type=check_positive,help='Relative speed of the gif (100 by default)')
+    parser.add_argument('-fps','--fraps',default=None,type=int,help='Frames per second')
+    parser.add_argument('-delsrc','--delete_source',action='store_true',help='Generate gif and remove source file')
+    
     args=parser.parse_args()
     file_extension = pathlib.Path(args.source).suffix
-
+ 
     if file_extension == '.webp':
         if args.start != 0.0 or args.end is not None or args.speed != 100 or args.size != 100:
             parser.error(Fore.RED+Style.BRIGHT+"-st/--start, -e/--end, -sz/--size and -spd/--speed specs not allowed for '.webp' to '.gif' conversion."+Fore.RESET+Style.RESET_ALL)
     else:
         probe = ffmpeg.probe(args.source)
         video_streams = [stream for stream in probe["streams"] if stream["codec_type"] == "video"]
-        
+ 
         if args.end:
             args.end = float(args.end)
         else:
             args.end = float(video_streams[0]['duration'])
-
+ 
         if args.start > args.end:
-            parser.error(Fore.RED+Style.BRIGHT+"start value must be smaller than end value."+Fore.RESET+Style.RESET_ALL) 
-    
+            parser.error(Fore.RED+Style.BRIGHT+"start value must be smaller than end value."+Fore.RESET+Style.RESET_ALL)
+ 
     gm(args)
-
-    
+ 
 def check_time(val):
     time = float(val)
     if time < 0.0:
         raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+f"time values must be equal or bigger than 0.00 ('{val}' is not valid)."+Fore.RESET+Style.RESET_ALL)
     return time
-
+ 
 def check_positive(val):
     ivalue = int(val)
     if ivalue <= 0:
         raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+f"speed and size values must be positive ('{val}' is not valid)."+Fore.RESET+Style.RESET_ALL)
     return ivalue
-
+ 
 def check_source_ext(file):
     supported_formats = ['.mp4','.avi','.mov','.wmv','.rm','.webp']
     file_extension = pathlib.Path(file).suffix
@@ -77,13 +77,13 @@ def check_source_ext(file):
     else:
         raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+f"FILE NOT FOUND: File '{file}' not found."+Fore.RESET+Style.RESET_ALL)
     return file
-
+ 
 def check_result_ext(file):
     file_extension = pathlib.Path(file).suffix
     if file_extension != '.gif':
         raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+f"result file must be '.gif' ('{file_extension}' is not valid)."+Fore.RESET+Style.RESET_ALL)
     return file
-
+ 
 def show(f):
     print("GENERATING VIEW...")
     try:
@@ -94,7 +94,7 @@ def show(f):
         w = sprite.width
         h = sprite.height
         window = pyglet.window.Window(width=w, height=h)
-
+ 
         @window.event
         def on_draw():
             sprite.draw()
@@ -102,14 +102,14 @@ def show(f):
         print(f"Successfully generated view from '{f}'.")
     except Exception as e:
         print("UNEXPECTED ERROR: ",str(e))
-
+ 
 def get_size_format(b, factor=1024, suffix="B"):
 	for unit in ["","K","M","G","T","P","E","Z"]:
 	    if b < factor:
 	        return f"{b:.4f}{unit}{suffix}"
 	    b /= factor
 	return f"{b:.4f}Y{suffix}"
-
+ 
 def gm(args):
     print(c_index+b_index+pyfiglet.figlet_format('MKGIF',font='graffiti')+Fore.RESET+Style.RESET_ALL)
     try:
@@ -124,7 +124,7 @@ def gm(args):
             clip.close()
             size = get_size_format(os.stat(args.destination).st_size)
             print(f"Created gif '{args.destination}' with size {size}.")
-            
+ 
         else:
             print("CONVERTING...")
             file = Image.open(args.source)
@@ -132,12 +132,16 @@ def gm(args):
             file.close()
             size = get_size_format(os.stat(args.destination).st_size)
             print(f"Created '{args.destination}' with size {size} from '{args.source}'.")
-            
+
+        if args.delete_source:
+            os.remove(args.source)
+            print(f"Removed file '{args.source}'.")
+ 
         if args.show:
             show(args.destination)
-                
+ 
     except Exception as e:
         print("UNEXPECTED ERROR: "+str(e))
-
+ 
 if __name__=='__main__':
     main()
