f55077ef869800b09b23fb06c5edd2f029d472e7
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-05-07 16:53:50 +0200

    Update mkgif8.py

--- mkgif8.py
+++ mkgif8.py
@@ -9,3 +9,13 @@ import random
 from colorama import Fore, init, Style
 import os
 from tqdm import tqdm
+
+
+init()
+color = {0:Fore.RED,1:Fore.GREEN,2:Fore.YELLOW,
+         3:Fore.BLUE,4:Fore.CYAN,5:Fore.MAGENTA,6:Fore.WHITE}
+ 
+bright = {0:Style.DIM,1:Style.NORMAL,2:Style.BRIGHT}
+ 
+c_index = color[random.randint(0,6)]
+b_index = bright[random.randint(0,2)]
