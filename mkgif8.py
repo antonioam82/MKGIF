819a59cf32e5217aff13d11aa5e3c6d2ced8fa44
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-05-07 16:52:58 +0200

    Create mkgif8.py

---
+++ mkgif8.py
@@ -0,0 +1,11 @@
+import pyfiglet
+import ffmpeg
+import pyglet
+from pyglet.window import key
+import argparse
+from PIL import Image
+import random
+#import subprocess
+from colorama import Fore, init, Style
+import os
+from tqdm import tqdm
